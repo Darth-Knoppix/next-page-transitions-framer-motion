b9bfe42fb1869c4c408b5ea029996e4c4f97c48d
Seth Corker <seth.corker@gmail.com>
2020-11-15 17:24:25 +0000

    remove space

--- components/StyledLink.js
+++ components/StyledLink.js
@@ -3,7 +3,7 @@ import React from "react";
 const StyledLink = React.forwardRef(({ onClick, href, children }, ref) => {
   return (
     <a
-      className="button  is-medium is-link"
+      className="button is-medium is-link"
       href={href}
       onClick={onClick}
       ref={ref}
